fe9cc5e47adf84713fab0794309fce6fbb251601
bhoomikaPrajapati <bhoomika824@gmail.com>
2018-05-04 19:18:05 +0530

    27 march 2018 -Solve search issue

--- app/src/main/java/com/search/activity/MainActivity.java
+++ app/src/main/java/com/search/activity/MainActivity.java
@@ -25,6 +25,12 @@ import com.search.adapter.AppsListAdapter;
 import com.search.customview.BaseRecyclerAdapter;
 import com.search.model.AppList;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -61,6 +67,8 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         rvAllApp.setAdapter(mAppsListAdapter);
         setAppData();
         setListener();
+
+
     }
 
     private void setListener() {
@@ -198,6 +206,49 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         if (null != activity.getCurrentFocus())
             imm.hideSoftInputFromWindow(activity.getCurrentFocus()
                     .getApplicationWindowToken(), 0);
+    }
+
+
+    private void writeToFile(String data) {
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(openFileOutput("config.txt", Context.MODE_PRIVATE));
+            outputStreamWriter.write(data);
+            outputStreamWriter.close();
+        }
+        catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
+    }
+
+
+    private String readFromFile() {
+
+        String ret = "";
+
+        try {
+            InputStream inputStream = this.openFileInput("config.txt");
+
+            if ( inputStream != null ) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String receiveString = "";
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ( (receiveString = bufferedReader.readLine()) != null ) {
+                    stringBuilder.append(receiveString);
+                }
+
+                inputStream.close();
+                ret = stringBuilder.toString();
+            }
+        }
+        catch (FileNotFoundException e) {
+            Log.e("login activity", "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e("login activity", "Can not read file: " + e.toString());
+        }
+
+        return ret;
     }
 
 
